8f46ff8e9fe5f516f7c01d89e2f4935135e9e0bb
Enzo Pain <nozbead@outlook.com>
2024-09-06 18:42:37 +0200

    add front skeleton

---
+++ front/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+function sendFile(e: Event) {
+  for (const file of e.currentTarget.files) {
+    const request = new XMLHttpRequest()
+    request.open('POST', 'http://localhost:8082', true)
+    request.upload.onprogress = (e) => {
+      console.log(e)
+    }
+    request.send(file)
+  }
+}
+</script>
+
+<template>
+  <input type="file" multiple @change="sendFile" />
+</template>
+
+<style scoped></style>
